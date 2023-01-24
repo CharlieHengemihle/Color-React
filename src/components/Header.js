d193d029705822a8afabdcf4f23df8903fe5dda9
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-24 15:21:42 -0700

    some css fun to make netlify checks run

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import './Header.css';
 export default function Header() {
   return (
     <header>
-      <NavLink exact={true} to="/">
+      <NavLink exact={true} to="/" className="home">
         Home
       </NavLink>
-      <NavLink exact={true} to="/RGB/49/3/176">
+      <NavLink exact={true} to="/RGB/49/3/176" className="blurp">
         Blurple!
       </NavLink>
-      <NavLink exact={true} to="/RGB/255/109/226">
+      <NavLink exact={true} to="/RGB/255/109/226" className="pank">
         Pank.
       </NavLink>
-      <NavLink exact={true} to="/RGB/155/136/136">
+      <NavLink exact={true} to="/RGB/155/136/136" className="grey">
         Greige :(
       </NavLink>
     </header>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import './Home.css';
 export default function Home() {
   return (
-    <body className="home">
+    <body className="home-display">
       <h1>Welcome Home</h1>
       <p>We Hope You Can Stay a While.....</p>
     </body>
